afa00071915b7f26796dc78c0623153ae06c8e5f
Kyle Riedemann <kylealanr@gmail.com>
2015-04-20 23:05:16 -0500

    example authentication using G+ working

---
+++ app/src/events/Events.js
@@ -0,0 +1,11 @@
+/**
+ * Created by kyle on 4/20/15.
+ */
+(function(){
+    'use strict';
+
+    // Prepare the 'users' module for subsequent registration of controllers and delegates
+    angular.module('events', [ 'ngMaterial' ]);
+
+
+})();
