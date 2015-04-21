c3123ceb3f7aa5e04c57cc020ba7f9f39039dfd5
Kyle Riedemann <kylealanr@gmail.com>
2015-04-20 20:49:17 -0500

    started refactoring material start users files

--- app/src/users/UserController.js
+++ app/src/users/UserController.js
@@ -3,7 +3,7 @@
   angular
        .module('users')
        .controller('UserController', [
-          'userService', '$mdSidenav', '$mdBottomSheet', '$log', '$q',
+          '$mdSidenav',
           UserController
        ]);
 
@@ -11,41 +11,46 @@
    * Main Controller for the Angular Material Starter App
    * @param $scope
    * @param $mdSidenav
-   * @param avatarsService
    * @constructor
    */
-  function UserController( userService, $mdSidenav, $mdBottomSheet, $log, $q) {
+  function UserController($mdSidenav) {
     var self = this;
 
     self.selected     = null;
-    self.users        = [ ];
+    self.users        = [
+        {
+            name: 'Admin',
+            avatar: 'admin',
+            content: 'Admin panel'
+        },
+        {
+            name: 'Log New',
+            avatar: 'notebook',
+            content: 'Clock in goes here'
+        },
+        {
+            name: 'Events',
+            avatar: 'archive',
+            content: 'Clock in here'
+        },
+        {
+            name: 'Settings',
+            avatar: 'settings',
+            content: "User settings"
+        },
+    ];
     self.selectUser   = selectUser;
     self.toggleList   = toggleUsersList;
-    self.share        = share;
-
-    // Load all registered users
-
-    userService
-          .loadAllUsers()
-          .then( function( users ) {
-            self.users    = [].concat(users);
-            self.selected = users[0];
-          });
 
     // *********************************
     // Internal methods
     // *********************************
 
     /**
-     * First hide the bottomsheet IF visible, then
      * hide or Show the 'left' sideNav area
      */
     function toggleUsersList() {
-      var pending = $mdBottomSheet.hide() || $q.when(true);
-
-      pending.then(function(){
         $mdSidenav('left').toggle();
-      });
     }
 
     /**
@@ -56,41 +61,6 @@
       self.selected = angular.isNumber(user) ? $scope.users[user] : user;
       self.toggleList();
     }
-
-    /**
-     * Show the bottom sheet
-     */
-    function share($event) {
-        var user = self.selected;
-
-        $mdBottomSheet.show({
-          parent: angular.element(document.getElementById('content')),
-          templateUrl: '/src/users/view/contactSheet.html',
-          controller: [ '$mdBottomSheet', UserSheetController],
-          controllerAs: "vm",
-          bindToController : true,
-          targetEvent: $event
-        }).then(function(clickedItem) {
-          clickedItem && $log.debug( clickedItem.name + ' clicked!');
-        });
-
-        /**
-         * Bottom Sheet controller for the Avatar Actions
-         */
-        function UserSheetController( $mdBottomSheet ) {
-          this.user = user;
-          this.items = [
-            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
-            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
-            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
-            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
-          ];
-          this.performAction = function(action) {
-            $mdBottomSheet.hide(action);
-          };
-        }
-    }
-
   }
 
 })();
