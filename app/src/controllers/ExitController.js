5bc0450fde3062441f3af702fedbadcf84458f35
Kyle Riedemann <kylealanr@gmail.com>
2015-04-26 20:16:02 -0500

    fixed issue where user would clear data and be unable to log in again

--- app/src/controllers/ExitController.js
+++ app/src/controllers/ExitController.js
@@ -6,8 +6,8 @@
 
     app.controller('ExitController', function ($scope, $rootScope, $cookies, $location) {
         $scope.clearData = function() {
-            $cookies.employeeId = "undefined";
-            $cookies.employeeName = "undefined";
+            $cookies.employeeId = undefined;
+            $cookies.employeeName = undefined;
             $scope.clearDataSuccessMessage = "Success!";
             console.log("cleared all data");
         };
